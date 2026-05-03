bd9e27886fcb03ca4b79f3c239ce2435ec5565e4
lorrayne-amorim <lorrayne_afdc@hotmail.com>
2026-05-03 15:50:00 -0300

    add: create contract configuration with address and ABI for Sepolia network

---
+++ src/config/contract.js
@@ -0,0 +1,6 @@
+import abi from '../abi/HistoricoQuilometragem.json'
+
+export const CONTRACT_ADDRESS = '0x34a284b45e5ae15aa44fa6dff299df06bb705e04'
+export const CONTRACT_ABI = abi
+export const SEPOLIA_CHAIN_ID = 11155111
+export const SEPOLIA_HEX = '0xaa36a7'
